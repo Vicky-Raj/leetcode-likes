3ee95f023b30daaee52e2a734992e0bb1618c0a5
VigneshR <vickylab2000@gmail.com>
2023-02-25 17:59:14 +0530

    added tampermonkey script

---
+++ userscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Leetcode
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://leetcode.com/tag/*
+// @icon         https://assets.leetcode.com/static_assets/public/icons/favicon-32x32.png
+// @grant        none
+// @require      file:///home/vicky/Documents/Projects/leetcode-likes/data.js
+// @require      file:///home/vicky/Documents/Projects/leetcode-likes/index.js
+// ==/UserScript==
